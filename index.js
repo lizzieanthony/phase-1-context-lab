1f69925ef8e8e3ca061e9fea8d7a676697a4421b
Lizzie Anthony <lizziewrab@me.com>
2021-12-01 11:54:44 -0600

    commit tree 5dbd1b42fd597da503cb769eb1f0e625091e73b2

--- index.js
+++ index.js
@@ -1,4 +1,50 @@
-/* Your Code Here */
+function createEmployeeRecord(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+
+function createEmployeeRecords(employees) {
+    return employees.map( employee => {
+        return createEmployeeRecord(employee)
+    })  
+}
+
+function createTimeInEvent(dateStamp) {
+    //console.log(dateStamp)
+    let [date, hour] = dateStamp.split(' ')
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date: date,
+    })
+    return this
+}
+
+function createTimeOutEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date: date,
+    })
+    return this
+}
+
+function hoursWorkedOnDate(date) {
+    let timeIn= this.timeInEvents.find(element => element.date === date).hour
+    let timeOut= this.timeOutEvents.find(element => element.date === date).hour
+    return (timeOut-timeIn)/100
+}
+
+function wagesEarnedOnDate(date){
+     return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +67,25 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(scrArray, firstName){
+    let employeeObj = scrArray.find(element => element.firstName === firstName)
+    return employeeObj
+}
+
+// function calculatePayroll(array){ 
+//     // console.log(allWagesFor)
+//     let sum = 0
+//     for (let i = 0; i < array.length; i++) {
+//         sum += allWagesFor(array[i]);
+//         //console.log(.array[i])
+//       }
+//       return sum
+//     }
+    
+    function calculatePayroll(arrayOfObjects){
+        let sum = 0;
+        for(let employee of arrayOfObjects){
+            sum += allWagesFor.call(employee);
+        }
+        return sum
+    }
